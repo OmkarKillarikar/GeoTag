29285d4c6a894c33fe76d609e9c8a6fa2ff23e9c
Omkar Killarikar <okmac@Omkars-MacBook-Air.local>
2018-08-13 21:58:27 +0530

    imports optimized

--- app/src/main/java/com/poc/okmac/geotag/fragments/MapFragment.java
+++ app/src/main/java/com/poc/okmac/geotag/fragments/MapFragment.java
@@ -17,7 +17,6 @@ import android.os.Vibrator;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
